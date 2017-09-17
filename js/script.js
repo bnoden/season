e9433f31d430b9a6bfc856ad5ed1ed9cea1ffa46
bnoden <brandroid369@gmail.com>
2017-09-16 20:48:14 -0500

    .

--- js/script.js
+++ js/script.js
@@ -1,30 +1,37 @@
-(()=>'strict-mode')();
+(() => 'strict-mode')();
 
 let vid;
 let spdShow;
 let speed;
+vid1 = document.getElementById('vid1');
+vid2 = document.getElementById('vid2');
+spdShow = document.querySelector('.speed-display');
 
 const speedup = () => {
-  vid = document.querySelector('.vid1');
-  spdShow = document.querySelector('.speed-display');
-  speed = vid.playbackRate;
-  vid.playbackRate = speed<32.0 ? speed+0.05 : 32.0;
-  spdShow.innerHTML = `speed: ${(vid.playbackRate*100).toFixed()}%`;
-  console.log(vid.playbackRate);
-}
+  speed = vid1.playbackRate;
+  vid1.playbackRate = speed < 32.0 ? speed + 0.05 : 32.0;
+  vid2.playbackRate = vid1.playbackRate;
+  spdShow.innerHTML = `speed: ${(vid1.playbackRate * 100).toFixed()}%`;
+  console.log(vid1.playbackRate);
+  console.log(vid2.playbackRate);
+};
 const slowdown = () => {
-  vid = document.querySelector('.vid1');
-  spdShow = document.querySelector('.speed-display');
-  speed = vid.playbackRate;
-  vid.playbackRate = speed>-8.9 ? speed-0.05 : -9.0;
-  spdShow.innerHTML = `speed: ${(vid.playbackRate*100).toFixed()}%`;
-  console.log(vid.playbackRate);
-}
+  speed = vid1.playbackRate;
+  vid1.playbackRate = speed > -8.9 ? speed - 0.05 : -9.0;
+  vid2.playbackRate = vid1.playbackRate
+  spdShow.innerHTML = `speed: ${(vid1.playbackRate * 100).toFixed()}%`;
+  console.log(vid1.playbackRate);
+  console.log(vid2.playbackRate);
+};
 
 (() => {
-  let i = 28;
-  while (i) {
-    speedup();
-    i--;
+  if (vid1.buffered && vid2.buffered) {
+    let i = 28;
+    vid1.play();
+    vid2.play();
+    while (i) {
+      speedup();
+      i--;
+    }
   }
 })();
