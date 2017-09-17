73efcffb2a75f1b3442460073fdd8e7c16bd0b1a
bnoden <brandroid369@gmail.com>
2017-09-16 21:50:59 -0500

    .

--- js/script.js
+++ js/script.js
@@ -11,29 +11,25 @@ spdShow = document.querySelector('.speed-display');
 const speedup = () => {
   speed = vid1.playbackRate;
   vid1.playbackRate = speed < 32.0 ? speed + 0.05 : 32.0;
-  vid2.playbackRate = vid1.playbackRate;
+  vid2.playbackRate = vid1.playbackRate*0.75;
   spdShow.innerHTML = `speed: ${(vid1.playbackRate * 100).toFixed()}%`;
-  console.log(vid1.playbackRate);
-  console.log(vid2.playbackRate);
 };
 const slowdown = () => {
   speed = vid1.playbackRate;
   vid1.playbackRate = speed > -8.9 ? speed - 0.05 : -9.0;
-  vid2.playbackRate = vid1.playbackRate
+  vid2.playbackRate = vid1.playbackRate*0.75;
   spdShow.innerHTML = `speed: ${(vid1.playbackRate * 100).toFixed()}%`;
-  console.log(vid1.playbackRate);
-  console.log(vid2.playbackRate);
 };
 
 (() => {
-  if (vid1.buffered && vid2.buffered) {
+  if (vid1.buffered && vid2.buffered && aud1.buffered) {
     aud1.play();
     let i = 28;
-    vid1.play();
-    vid2.play();
     while (i) {
       speedup();
       i--;
     }
+    vid1.play();
+    vid2.play();
   }
 })();
