15ea23220c3a91b6ef293f3997f4b5e5c112192f
bnoden <brandroid369@gmail.com>
2017-10-13 02:25:09 -0500

    refactor

--- js/script.js
+++ js/script.js
@@ -2,20 +2,14 @@
 
 let vid;
 let speed1, speed2;
-const vid1 = document.querySelector('#vid1');
-const vid2 = document.querySelector('#vid2');
-const aud1 = document.querySelector('#aud1');
-const spdDisplay1 = document.querySelector('.speed-display-1');
-const spdDisplay2 = document.querySelector('.speed-display-2');
-const speedSlider1 = document.querySelector('#speedSlider-1');
-const speedSlider2 = document.querySelector('#speedSlider-2');
-const opacitySlider1 = document.querySelector('#opacitySlider-1');
-const opacitySlider2 = document.querySelector('#opacitySlider-2');
-const ctrl = document.querySelector('.ctrl');
-const maxSpeed = speedSlider1.max;
-const minSpeed = speedSlider1.min;
 const qs = el => document.querySelector(el);
 const qsa = el => document.querySelectorAll(el);
+const vid1 = qs('#vid1');
+const vid2 = qs('#vid2');
+const aud1 = qs('#aud1');
+const ctrl = qsa('.ctrl');
+const maxSpeed = qs('#speedSlider-1').max;
+const minSpeed = qs('#speedSlider-1').min;
 
 const speedSlide = (v, ss) => {
   v.playbackRate = ss.value;
@@ -29,18 +23,18 @@ const setSpeedDisplay = (el, display, str) => {
   )}</span>${(el.playbackRate * 100).toFixed()}%`;
 };
 const slide1 = () => {
-  speedSlide(vid1, speedSlider1);
-  setSpeedDisplay(vid1, spdDisplay1, 'speed one:');
+  speedSlide(vid1, qs('#speedSlider-1'));
+  setSpeedDisplay(vid1, qs('.speed-display-1'), 'speed one:');
 };
 const slide2 = () => {
-  speedSlide(vid2, speedSlider2);
-  setSpeedDisplay(vid2, spdDisplay2, 'speed two:');
+  speedSlide(vid2, qs('#speedSlider-2'));
+  setSpeedDisplay(vid2, qs('.speed-display-2'), 'speed two:');
 };
 const slide3 = () => {
-  opacitySlide(vid1, opacitySlider1);
+  opacitySlide(vid1, qs('#opacitySlider-1'));
 };
 const slide4 = () => {
-  opacitySlide(vid2, opacitySlider2);
+  opacitySlide(vid2, qs('#opacitySlider-2'));
 };
 
 const speedup = () => {
@@ -48,20 +42,20 @@ const speedup = () => {
   speed2 = vid2.playbackRate;
   vid2.playbackRate = speed2 < maxSpeed ? speed2 + 0.01 : maxSpeed;
   vid1.playbackRate = speed1 < maxSpeed ? speed1 + 0.01 : maxSpeed;
-  speedSlider1.value = vid1.playbackRate;
-  speedSlider2.value = vid2.playbackRate;
-  setSpeedDisplay(vid1, spdDisplay1, 'speed one:');
-  setSpeedDisplay(vid2, spdDisplay2, 'speed two:');
+  qs('#speedSlider-1').value = vid1.playbackRate;
+  qs('#speedSlider-2').value = vid2.playbackRate;
+  setSpeedDisplay(vid1, qs('.speed-display-1'), 'speed one:');
+  setSpeedDisplay(vid2, qs('.speed-display-2'), 'speed two:');
 };
 const slowdown = () => {
   speed1 = vid1.playbackRate;
   speed2 = vid2.playbackRate;
   vid2.playbackRate = speed2 > minSpeed ? speed2 - 0.01 : minSpeed;
   vid1.playbackRate = speed1 > minSpeed ? speed1 - 0.01 : minSpeed;
-  speedSlider1.value = vid1.playbackRate;
-  speedSlider2.value = vid2.playbackRate;
-  setSpeedDisplay(vid1, spdDisplay1, 'speed one:');
-  setSpeedDisplay(vid2, spdDisplay2, 'speed two:');
+  qs('#speedSlider-1').value = vid1.playbackRate;
+  qs('#speedSlider-2').value = vid2.playbackRate;
+  setSpeedDisplay(vid1, qs('.speed-display-1'), 'speed one:');
+  setSpeedDisplay(vid2, qs('.speed-display-2'), 'speed two:');
 };
 
 const reveal = (el, timeout) => {
@@ -75,73 +69,21 @@ const hide = (el, timeout) => {
     el.style.opacity = 0;
   }, timeout);
 };
-const disableAll = () => {
-  // qs('.btn-blastoff').disabled = true;
-  qs('.btn-shrink-2').disabled = true;
-  qs('.btn-shrink-1').disabled = true;
-  qs('.btn-grow-2').disabled = true;
-  qs('.btn-grow-1').disabled = true;
-  qs('.btn-slow').disabled = true;
-  qs('.btn-fast').disabled = true;
-  qs('.btn-swap-z').disabled = true;
-  qs('.btn-swap-s').disabled = true;
-  speedSlider1.disabled = true;
-  speedSlider2.disabled = true;
-  opacitySlider1.disabled = true;
-  opacitySlider2.disabled = true;
+const toggleControls = bool => {
+  for (let controls of ctrl) {
+    controls.disabled = !typeof bool ? !controls.disabled : bool;
+  }
 };
-const enableAll = () => {
-  // qs('.btn-blastoff').disabled = false;
-  qs('.btn-shrink-2').disabled = false;
-  qs('.btn-shrink-1').disabled = false;
-  qs('.btn-grow-2').disabled = false;
-  qs('.btn-grow-1').disabled = false;
-  qs('.btn-slow').disabled = false;
-  qs('.btn-fast').disabled = false;
-  qs('.btn-swap-z').disabled = false;
-  qs('.btn-swap-s').disabled = false;
-  speedSlider1.disabled = false;
-  speedSlider2.disabled = false;
-  opacitySlider1.disabled = false;
-  opacitySlider2.disabled = false;
-};
+
 const hideAll = () => {
-  if (qs('.ctr').style.visibility === 'visible') {
-    spdDisplay1.style.opacity = 0;
-    spdDisplay2.style.opacity = 0;
-    // qs('.btn-blastoff').style.opacity = 0;
-    qs('.btn-shrink-2').style.opacity = 0;
-    qs('.btn-shrink-1').style.opacity = 0;
-    qs('.btn-grow-2').style.opacity = 0;
-    qs('.btn-grow-1').style.opacity = 0;
-    qs('.btn-slow').style.opacity = 0;
-    qs('.btn-fast').style.opacity = 0;
-    qs('.btn-swap-z').style.opacity = 0;
-    qs('.btn-swap-s').style.opacity = 0;
-    speedSlider1.style.opacity = 0;
-    speedSlider2.style.opacity = 0;
-    opacitySlider1.style.opacity = 0;
-    opacitySlider2.style.opacity = 0;
+  for (let controls of ctrl) {
+    controls.style.opacity = 0;
   }
 };
 
 const revealAll = () => {
-  if (qs('.ctr').style.visibility === 'visible') {
-    spdDisplay1.style.opacity = 1;
-    spdDisplay2.style.opacity = 1;
-    // qs('.btn-blastoff').style.opacity = 1;
-    qs('.btn-shrink-2').style.opacity = 1;
-    qs('.btn-shrink-1').style.opacity = 1;
-    qs('.btn-grow-2').style.opacity = 1;
-    qs('.btn-grow-1').style.opacity = 1;
-    qs('.btn-slow').style.opacity = 1;
-    qs('.btn-fast').style.opacity = 1;
-    qs('.btn-swap-z').style.opacity = 1;
-    qs('.btn-swap-s').style.opacity = 1;
-    speedSlider1.style.opacity = 1;
-    speedSlider2.style.opacity = 1;
-    opacitySlider1.style.opacity = 1;
-    opacitySlider2.style.opacity = 1;
+  for (let controls of ctrl) {
+    controls.style.opacity = 1;
   }
 };
 
@@ -152,66 +94,66 @@ const showUI = t => {
     vid1.style.opacity = 0.5;
     vid2.style.opacity = 0.5;
   }, t);
-  reveal(spdDisplay1, t);
-  reveal(spdDisplay2, t * 2);
+  reveal(qs('.speed-display-1'), t);
+  reveal(qs('.speed-display-2'), t * 2);
   reveal(qs('.btn-slow'), t * 3);
   reveal(qs('.btn-fast'), t * 4);
   reveal(qs('.btn-swap-z'), t * 3);
   reveal(qs('.btn-swap-s'), t * 4);
-  reveal(speedSlider1, t * 5);
-  reveal(speedSlider2, t * 6);
-  reveal(opacitySlider1, t * 7);
-  reveal(opacitySlider2, t * 8);
+  reveal(qs('#speedSlider-1'), t * 5);
+  reveal(qs('#speedSlider-2'), t * 6);
+  reveal(qs('#opacitySlider-1'), t * 7);
+  reveal(qs('#opacitySlider-2'), t * 8);
   setTimeout(() => {
-    opacitySlider1.value = 0.3;
-    vid1.style.opacity = opacitySlider1.value;
+    qs('#opacitySlider-1').value = 0.3;
+    vid1.style.opacity = qs('#opacitySlider-1').value;
   }, t * 9);
   setTimeout(() => {
-    opacitySlider2.value = 0.3;
-    vid2.style.opacity = opacitySlider2.value;
+    qs('#opacitySlider-2').value = 0.3;
+    vid2.style.opacity = qs('#opacitySlider-2').value;
   }, t * 10);
   setTimeout(() => {
-    opacitySlider1.value = 0.7;
-    vid1.style.opacity = opacitySlider1.value;
+    qs('#opacitySlider-1').value = 0.7;
+    vid1.style.opacity = qs('#opacitySlider-1').value;
   }, t * 11);
   setTimeout(() => {
-    opacitySlider2.value = 0.7;
-    vid2.style.opacity = opacitySlider2.value;
+    qs('#opacitySlider-2').value = 0.7;
+    vid2.style.opacity = qs('#opacitySlider-2').value;
   }, t * 12);
   setTimeout(() => {
-    opacitySlider1.value = 1;
-    vid1.style.opacity = opacitySlider1.value;
+    qs('#opacitySlider-1').value = 1;
+    vid1.style.opacity = qs('#opacitySlider-1').value;
   }, t * 13);
   setTimeout(() => {
-    opacitySlider2.value = 1;
-    vid2.style.opacity = opacitySlider2.value;
+    qs('#opacitySlider-2').value = 1;
+    vid2.style.opacity = qs('#opacitySlider-2').value;
   }, t * 14);
   setTimeout(() => {
     speed2 = 1.8;
-    speedSlider2.value = speed2;
-    vid2.playbackRate = speedSlider2.value;
-    setSpeedDisplay(vid2, spdDisplay2, 'speed two:');
+    qs('#speedSlider-2').value = speed2;
+    vid2.playbackRate = qs('#speedSlider-2').value;
+    setSpeedDisplay(vid2, qs('.speed-display-2'), 'speed two:');
   }, t * 15);
   setTimeout(() => {
     speed1 = 2.1;
-    speedSlider1.value = speed1;
-    vid1.playbackRate = speedSlider1.value;
-    setSpeedDisplay(vid1, spdDisplay1, 'speed one:');
+    qs('#speedSlider-1').value = speed1;
+    vid1.playbackRate = qs('#speedSlider-1').value;
+    setSpeedDisplay(vid1, qs('.speed-display-1'), 'speed one:');
   }, t * 16);
   setTimeout(() => {
     allowSwap = 0b1;
-    enableAll();
+    toggleControls();
   }, t * 17);
-  hide(spdDisplay1, t * 18);
-  hide(spdDisplay2, t * 18.2);
+  hide(qs('.speed-display-1'), t * 18);
+  hide(qs('.speed-display-2'), t * 18.2);
   hide(qs('.btn-slow'), t * 18.4);
   hide(qs('.btn-fast'), t * 18.6);
   hide(qs('.btn-swap-z'), t * 18.8);
   hide(qs('.btn-swap-s'), t * 19);
-  hide(speedSlider1, t * 19.2);
-  hide(speedSlider2, t * 19.4);
-  hide(opacitySlider1, t * 19.7);
-  hide(opacitySlider2, t * 20);
+  hide(qs('#speedSlider-1'), t * 19.2);
+  hide(qs('#speedSlider-2'), t * 19.4);
+  hide(qs('#opacitySlider-1'), t * 19.7);
+  hide(qs('#opacitySlider-2'), t * 20);
   qs('.ctr').style.visibility = 'visible';
 };
 
@@ -376,7 +318,7 @@ const delight = () => {
     qs('.ctr').style.zIndex = 30;
     vid1.style.visibility = 'visible';
     vid2.style.visibility = 'visible';
-    qs('.instructions').style.display = 'none';
+    qs('.message').style.visibility = 'hidden';
     if (vid2.buffered && vid1.buffered && aud1.buffered) {
       aud1.play();
       setTimeout(() => {
@@ -402,6 +344,6 @@ const delight = () => {
 };
 
 (() => {
-  disableAll();
+  toggleControls();
   qs('.ctr').style.visibility = 'hidden';
 })();
